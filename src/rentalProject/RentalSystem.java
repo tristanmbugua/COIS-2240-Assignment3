59d6fd0c0f4c0952c2436e3fa9681ccf3a91f242
Tristan Mbugua <128255762+tristanmbugua@users.noreply.github.com>
2025-04-01 15:18:26 -0400

    Rent/Return Vehicle Validation
    Rent vehicle and return vehicle methods are tested now. This required updating the RentalSystemApp class.
    
    Task 2 - Subtask 2

--- src/rentalProject/RentalSystem.java
+++ src/rentalProject/RentalSystem.java
@@ -70,27 +70,31 @@ public class RentalSystem {
     	return saveCustomer(customer);
     }
 
-    public void rentVehicle(Vehicle vehicle, Customer customer, LocalDate date, double amount) {
+    public boolean rentVehicle(Vehicle vehicle, Customer customer, LocalDate date, double amount) {
         if (vehicle.getStatus() == Vehicle.VehicleStatus.AVAILABLE) {
             vehicle.setStatus(Vehicle.VehicleStatus.RENTED);
             saveRecord(new RentalRecord(vehicle, customer, date, amount, "RENT"));
             System.out.println("Vehicle rented to " + customer.getCustomerName());
+            return true;
         }
         else {
             System.out.println("Vehicle is not available for renting.");
+            return false;
         }
     }
-
-    public void returnVehicle(Vehicle vehicle, Customer customer, LocalDate date, double extraFees) {
+    public boolean returnVehicle(Vehicle vehicle, Customer customer, LocalDate date, double extraFees) {
         if (vehicle.getStatus() == Vehicle.VehicleStatus.RENTED) {
             vehicle.setStatus(Vehicle.VehicleStatus.AVAILABLE);
             saveRecord(new RentalRecord(vehicle, customer, date, extraFees, "RETURN"));
             System.out.println("Vehicle returned by " + customer.getCustomerName());
+            return true;
         }
         else {
             System.out.println("Vehicle is not rented.");
+            return false;
         }
-    }    
+    }
+
 
     public void displayVehicles(boolean onlyAvailable) {
     	System.out.println("|     Type         |\tPlate\t|\tMake\t|\tModel\t|\tYear\t|");
--- src/testing/VehicleRentalTest.java
+++ src/testing/VehicleRentalTest.java
@@ -8,6 +8,10 @@ import org.junit.jupiter.api.*;
 
 import rentalProject.*;
 
+import java.time.LocalDate;
+
+import java.lang.reflect.*;
+
 
 class VehicleRentalTest {
 	//Vehicle License Plate Validation
@@ -35,4 +39,38 @@ class VehicleRentalTest {
 		
 		Assertions.assertThrows(IllegalArgumentException.class, () -> { v.setLicensePlate("ZZZ99"); });
 	}
+
+	@Test
+		void testRentAndReturnVehicle() {
+			//Instantiate Objects
+			Vehicle v = new Car("Tristan", "Tristan", 1, 1);
+			Customer c = new Customer(1, "Tristan");
+			
+			//Verify the vehicle is available
+			Assertions.assertTrue(v.getStatus().equals(Vehicle.VehicleStatus.AVAILABLE));
+			
+			//Retrieve RentalSystem instance
+			RentalSystem r = RentalSystem.getInstance();
+			
+			//rentVehicle() method
+				//Rent Vehicle, verify success
+				Assertions.assertTrue(r.rentVehicle(v, c, LocalDate.now(), 1));
+				
+				//Verify the vehicle is rented
+				Assertions.assertTrue(v.getStatus().equals(Vehicle.VehicleStatus.RENTED));
+				
+				//Rent Vehicle, verify failure
+				Assertions.assertFalse(r.rentVehicle(v, c, LocalDate.now(), 1));
+			
+			//returnVehicle() method		
+				//Return Vehicle, verify success
+				Assertions.assertTrue(r.returnVehicle(v, c, LocalDate.now(), 1));
+				
+				//Verify the vehicle is available
+				Assertions.assertTrue(v.getStatus().equals(Vehicle.VehicleStatus.AVAILABLE));
+				
+				//Return Vehicle, verify failure
+				Assertions.assertFalse(r.returnVehicle(v, c, LocalDate.now(), 1));
+		}
+
 }
